3c8353673ad09bc83075942535ef2843aa9f0ba0
Marcos Vergara <70786131+im-marcosv@users.noreply.github.com>
2021-01-08 22:54:53 -0300

    Add files via upload
    timeit added

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,13 +1,15 @@
+from timeit import timeit
+
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [8,0,0,0,0,0,0,0,0],
+    [0,0,3,6,0,0,0,0,0],
+    [0,7,0,0,9,0,2,0,0],
+    [0,5,0,0,0,7,0,0,0],
+    [0,0,0,0,4,5,7,0,0],
+    [0,0,0,1,0,0,0,3,0],
+    [0,0,1,0,0,0,0,6,8],
+    [0,0,8,5,0,0,0,1,0],
+    [0,9,0,0,0,0,4,0,0]
 ]
 #Print the board with -- and ||
 def print_board(board):
@@ -62,6 +64,9 @@ def solve(board):
     return False
     
 print_board(board)
-solve(board)
+print()
+print(str(timeit('solve(board)', 'from __main__ import solve, board')) + " segundos para resolver el 'Sudoku más dificil del mundo'")
+
 print("\n- - - - - - - - - - - - -\n")
+
 print_board(board)
